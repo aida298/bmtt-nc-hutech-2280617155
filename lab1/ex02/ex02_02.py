6cd67c5fba257b3abcb3a351801babbf607b8b90
Hoang Anh Tu <tram519809@gmail.com>
2025-06-09 14:37:42 +0700

    Delete lab1 directory

--- lab1/ex02/ex02_02.py
+++
@@ -1,4 +0,0 @@
-
-bankinh = float(input("nhap ban kinh : "))
-dientich = 3.14 * (bankinh * 2)
-print("ban kinh hinh tron la : ",dientich)
--- lab1/ex03/ex03_03.py
+++
@@ -1,7 +0,0 @@
-def tttl(lst):
-    return tuple(lst)
-inputl = input("nhap danh sach: ")
-so = list(map(int, inputl(',')))
-my = tttl(so)
-print("list : ", so)
-print("tuple tu list : ", my)
